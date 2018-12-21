104862e8ef7e491ce34097fd35fbeeed1e0234bf
albert.xing <265569633@qq.com>
2018-12-21 10:46:50 +0800

    publish git server on windows server

---
+++ Windows/Readme.txt
@@ -0,0 +1,17 @@
+/********************** gitblit install **************************/
+1、install java sdk and add envparams
+    A、 add new JAVA_HOME  D:\Program Files (x86)\Java\jdk1.6.0_21 (your own installer path)
+    B、 edit    PATH       %JAVA_HOME%/bin;%JAVA_HOME%/jre/bin
+    C、 add new CLASSPATH  .;%JAVA_HOME%/lib/dt.jar;%JAVA_HOME%/lib/tools.jar
+2、unzip gitblit-1.*.zip, download from: http://www.gitblit.com/
+3、add git repository folder
+/********************** gitblit config **************************/
+1、Copy gitblit.properties file to gitblit-1.*/data folder
+2、Run  gitblit-1.*/gitblit.cmd to check gitblit site run wheather right or not
+   gitblit site is setting in the step.1, e.g.http://localhost:58909
+3、Run installService.cmd to add to windowsservice and set it run automaticly
+   Tips:
+    A、Befor you run the installService.cmd file you should add some node like SET CD=C:\Git\gitblit-1.8.0 after SET ARCH=amd64
+    B、Maybe you can  also change --StartParams="--storePassword;gitblit;--baseFolder;%CD%\data" ^ to be --StartParams="" ^
+4、You can see git is added into the windows services and running auto
+
---
+++ Windows/installService.cmd
@@ -0,0 +1,40 @@
+@REM Install Gitblit as a Windows service.
+
+@REM gitblitw.exe (prunmgr.exe) is a GUI application for monitoring 
+@REM and configuring the Gitblit procrun service.
+@REM
+@REM By default this tool launches the service properties dialog
+@REM but it also has some other very useful functionality.
+@REM
+@REM http://commons.apache.org/daemon/procrun.html
+
+@REM arch = x86, amd64, or ia32
+SET ARCH=amd64
+SET CD=C:\Git\gitblit-1.8.0
+
+@REM Be careful not to introduce trailing whitespace after the ^ characters.
+@REM Use ; or # to separate values in the --StartParams parameter.
+@REM		 --StartParams="--storePassword;gitblit;--baseFolder;%CD%\data" ^
+"%CD%\%ARCH%\gitblit.exe"  //IS//gitblit ^
+		 --DisplayName="gitblit" ^
+		 --Description="a pure Java Git solution" ^
+		 --Startup=auto ^
+		 --LogPath="%CD%\logs" ^
+		 --LogLevel=INFO ^
+		 --LogPrefix=gitblit ^
+		 --StdOutput=auto ^
+		 --StdError=auto ^
+		 --StartPath="%CD%" ^
+		 --StartClass=org.moxie.MxLauncher ^
+		 --StartMethod=main ^
+		 --StartParams="--storePassword;gitblit;--baseFolder;%CD%\data" ^
+		 --StartMode=jvm ^
+		 --StopPath="%CD%" ^
+		 --StopClass=org.moxie.MxLauncher ^
+		 --StopMethod=main ^
+		 --StopParams="--stop;--baseFolder;%CD%\data" ^
+		 --StopMode=jvm ^
+		 --Classpath="%CD%\gitblit.jar" ^
+		 --Jvm=auto ^
+		 --JvmMx=1024
+		 
